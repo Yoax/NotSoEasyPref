fa190f581bd27aff4a2cbacf7dfffc91621ff049
Yoan Pouzet <67773078+Yoax@users.noreply.github.com>
2022-05-14 10:22:39 +0200

    Publication
    C'est tout bon, c'est tout chaud, c'est NotSoEasyPref V0.5 !!!

--- NotSoEasyPref_v0.5.py
+++ NotSoEasyPref_v0.5.py
@@ -18,42 +18,64 @@ from selenium.webdriver.common.by import By
 import time
 from datetime import datetime, date
 
-# Début du script
+# Déclaration des variables
+dictionnaireDemarches = {
+    "depot_1er_TDS": "https://www.seine-maritime.gouv.fr/booking/create/50382/0",
+    "renouvelement_TDS": "https://www.seine-maritime.gouv.fr/booking/create/50389/0",
+    "retrait_TDS": "https://www.seine-maritime.gouv.fr/booking/create/50420/0",
+    "retrait_recepisse": "https://www.seine-maritime.gouv.fr/booking/create/50416/0",
+    "retrait_DCEM_TVR_TIV": "https://www.seine-maritime.gouv.fr/booking/create/51406/0",
+    "regularisation_sejour": "https://www.seine-maritime.gouv.fr/booking/create/47116/0",
+}
+tempsAttente = 20 ###en secondes
+
+jour = date.today().strftime("%m/%d/%y") ###format MM/JJ/AA - ce format aide à s'organiser dans des fichiers car il permet de trier alphabétiquement et chronologiquement en un clic
+
+# Lancement du WebDriver
+driver = webdriver.Firefox()
+
+# Script
 print(
-    "\n---\nn0750345ypr3f : un script pour (sur)veiller l'accès aux droits des étrangers\n~~~~~~~~\n"
-    "Patientez 1 minute ! Ne fermez pas Firefox, le petit robot travaille dur !\n"
-    "Quand la fenêtre de Firefox se fermera, vous aurez un petit message !\n\n"
+    "\n~~~~~~~~\n"
+    "n0750345ypr3f : un script pour (sur)veiller l'accès aux droits des étrangers"
+    "\n~~~~~~~~\n"
+    "Patientez 5 minutes ! Ne fermez pas Firefox, le petit robot travaille dur !\n"
+    "Quand la fenêtre de Firefox se fermera, ce sera terminé !"
+    "\n~~~~~~~~\n"
 )
 
-# Initialisation du WebDriver
-driver = webdriver.Firefox()
-driver.get("https://www.seine-maritime.gouv.fr/booking/create/50389/0")
+for i in dictionnaireDemarches:
+    ## Nouveau tour
+    nomDemarche = str(i)
+    urlDemarche = str(dictionnaireDemarches.get(i))
+    driver.get(urlDemarche)
+    heure = datetime.now().strftime("%H:%M:%S") ###format HH:MM:SS
 
-# Récupération et formatage de la date et de l'heure pour l'export
-heure = datetime.now().strftime("%H:%M:%S")
-jour = date.today().strftime("%m/%d/%y")
+    ## Coche le bouton d'acceptation
+    elem = driver.find_element(By.NAME, "condition")
+    elem.send_keys(Keys.SPACE)
 
-# Coche le bouton d'acceptation
-elem = driver.find_element(By.NAME, "condition")
-elem.send_keys(Keys.SPACE)
+    ## Appuie sur le bouton pour aller à la page suivante
+    elem = driver.find_element(By.NAME, "nextButton")
+    elem.send_keys(Keys.SPACE)
 
-# Appuie sur le bouton pour aller à la page suivante
-elem = driver.find_element(By.NAME, "nextButton")
-elem.send_keys(Keys.SPACE)
+    ## Récupère et compare le texte issu de la requête JQuery du serveur
+    time.sleep(tempsAttente) ### nécessaire pour éviter que cette action se lance avant que la seconde page ait chargée (améliorable)
+    elem = driver.find_element(By.ID, "FormBookingCreate").text
+    if elem == "Il n'existe plus de plage horaire libre pour votre demande de rendez-vous. Veuillez recommencer ultérieurement.":
+        disponibilite = 0
+        print(nomDemarche, " : il n'y a plus de disponibilité")
+    else:
+        disponibilite = 1
+        print(nomDemarche, " : DES CRENEAUX SONT DISPOS !!!")
+        
+    fichier = open("rapport" + ".csv", "a")
+    fichier.write(str(jour) + "," + str(heure) + "," + nomDemarche + "," + str(disponibilite) + "\n")
 
-# Récupère et compare le texte issu de la requête JQuery du serveur
-time.sleep(20) # nécessaire pour éviter que cette action se lance avant que la seconde page ait chargée (améliorable)
-elem = driver.find_element(By.ID, "FormBookingCreate").text
-if elem == "Il n'existe plus de plage horaire libre pour votre demande de rendez-vous. Veuillez recommencer ultérieurement.":
-    disponibilite = 0
-else:
-    disponibilite = 1
-
-# Ferme le WebDriver et, par extension, Firefox
+# Et c'est terminé !
 driver.close()
-
-# Ajoute les résultats du parsing à un fichier csv pour par la suite manipuler les données simplement
-fichier = open("step1Stats" + ".csv", "a")
-fichier.write(str(jour) + "," + str(heure) + "," + str(disponibilite) + "\n")
-
-print("TERMINÉ AVEC BRIO !（＾ω＾)\n---\n")
+print(
+    "\n~~~~~~~~\n"
+    "TERMINÉ AVEC BRIO !（＾ω＾)"
+    "\n~~~~~~~~\n"
+)
