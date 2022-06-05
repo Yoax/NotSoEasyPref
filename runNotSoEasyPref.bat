9a1e580de864990da2f98e8a3147a6712667d037
Yoan Pouzet <67773078+Yoax@users.noreply.github.com>
2022-06-05 16:53:16 +0200

    Add files via upload

---
+++ runNotSoEasyPref.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+TITLE NotSoEasyPref
+cd C:\Users\Yoan\OneDrive\Documents\MyOwnPrograms\NotSoEasyPref\
+"C:\Users\Yoan\AppData\Local\Microsoft\WindowsApps\python3.exe" "C:\Users\Yoan\OneDrive\Documents\MyOwnPrograms\NotSoEasyPref\script.py"
