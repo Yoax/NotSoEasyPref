b9f47caa387b7c9ab8a8171415cb3a57f97cece0
Yoan Pouzet <67773078+Yoax@users.noreply.github.com>
2022-06-11 00:48:33 +0200

    Delete runNotSoEasyPref.bat

--- runNotSoEasyPref.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-TITLE NotSoEasyPref
-cd C:\Users\Yoan\OneDrive\Documents\MyOwnPrograms\NotSoEasyPref\
-"C:\Users\Yoan\AppData\Local\Microsoft\WindowsApps\python3.exe" "C:\Users\Yoan\OneDrive\Documents\MyOwnPrograms\NotSoEasyPref\script.py"
