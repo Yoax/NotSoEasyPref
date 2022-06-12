691ae914208a0e4411567c08b6f7ce7bc08a35bd
Yoan Pouzet <67773078+Yoax@users.noreply.github.com>
2022-06-12 08:02:35 +0200

    Amélioration des outils de navigation
    - Ajout d'un défilement horizontal et vertical

--- graphique.py
+++ graphique.py
@@ -1,8 +1,23 @@
+"""
+Copyright 2022 Yoan POUZET
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import datetime
 import csv
 import bokeh
+import os
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import DatetimeTickFormatter
+from bokeh.models import DatetimeTickFormatter, WheelZoomTool, HoverTool
 from bokeh.themes import built_in_themes
 from bokeh.io import curdoc
 
@@ -82,9 +97,8 @@ p.legend.click_policy = "hide"
 p.legend.background_fill_color = "white"
 p.legend.background_fill_alpha = 0.2
 ## du titre
-p.title.text_font_size = "22px"
+p.title.text_font_size = "16px"
 p.title.align = "center"
-p.title.text_color = "grey"
 ## du theme
 output_file("bokeh.html")
 curdoc().theme = 'night_sky'
@@ -101,8 +115,19 @@ p.axis.minor_tick_out = 6
 p.xaxis[0].formatter = DatetimeTickFormatter(hours = ['%H:%M'], days = ['%A %d %B'])
 ## de l'arriere grille
 p.ygrid.grid_line_alpha = 0.8
-p.ygrid.grid_line_dash = [2, 4]
+p.ygrid.grid_line_dash = [4, 8]
 p.xgrid.band_fill_color = "olive"
 p.xgrid.band_fill_alpha = 0.1
 
+# On ajoute des outils pour parfaire la consultation du graphique
+## les défilements horizontaux pour contrôler les espacements entre les données temporelles
+defilementHorizontal = WheelZoomTool(dimensions = 'width', description = "Contrôler à la molette l'abcisse")
+defilementVertical = WheelZoomTool(dimensions = 'height', description = "Contrôler à la molette l'ordonnée")
+p.add_tools(defilementHorizontal, defilementVertical)
+p.toolbar.active_scroll = defilementHorizontal
+
+## on affiche le graphique
 show(p)
+
+## on le déplace vers le dossier qui est mis en ligne
+os.replace("/home/yoan/bokeh.html", "/home/yoan/NotSoEasyPref/Data/graphique.html")
